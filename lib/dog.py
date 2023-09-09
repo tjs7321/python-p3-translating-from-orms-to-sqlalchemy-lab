b60f21e61ad6aa2d8b48a89dcc00257007bafccc
Theodore Smith <tjs7321@gmail.com>
2023-09-09 00:31:58 -0400

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,33 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog)
+    return ([dog for dog in dogs])
 
 def find_by_name(session, name):
-    pass
+    query = session.query(Dog).filter(Dog.name.like(f'%{name}%'))
+    for instance in query:
+        return instance
 
 def find_by_id(session, id):
-    pass
+    query = session.query(Dog).filter(Dog.id == id)
+    for instance in query:
+        return instance
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    query = session.query(Dog).filter(Dog.name.like(f'%{name}%'),
+        Dog.breed.like(f'%{breed}%'))
+    for instance in query:
+        return instance
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
